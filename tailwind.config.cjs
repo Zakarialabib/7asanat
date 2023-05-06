f6f32547d295e6115a974141769e0b9f8a0893fa
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-05-06 23:45:09 +0100

    UI : New green color called shamrock

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+      'shamrock': {
+        '50': '#ecfdf7',
+        '100': '#d1faeb',
+        '200': '#a7f3d7',
+        '300': '#6ee7bb',
+        '400': '#34d399',
+        '500': '#10b97b',
+        '600': '#059661',
+        '700': '#04784e',
+        '800': '#065f3f',
+        '900': '#064e34',
+        '950': '#022c1d',
+      },
+    },
+
+
+    }
   },
   plugins: []
 };
